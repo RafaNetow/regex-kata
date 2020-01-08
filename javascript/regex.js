0fd12844cb50bcf973cdeed158739409568459d2
Rafael Sequeiros <rsequeiros@supports-MacBook-Pro.local>
2020-01-08 10:20:21 -0600

    added example on js

---
+++ javascript/regex.js
@@ -0,0 +1,12 @@
+//Hello World
+let regex = /hello/
+let str = 'hello world'
+let result = regex.test(str)
+console.log(result)
+
+//Testing DateTime
+let notBinary = /[^01]/
+console.log(notBinary.test('1100100010100110'))
+// → false
+console.log(notBinary.test('1100100010200110'))
+// → true
